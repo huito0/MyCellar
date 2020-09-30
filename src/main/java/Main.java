7f45692798953f54094a80ae20424ed1adb887d0
German Konrad <gkonrad@htc-cs.ru>
2020-09-30 08:29:26 +0400

    Докостылены методы и даже что-то нмного работает

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        /**Рандомное заполнение погреба **/
+        System.out.println(Vault.generateArrayJars());
+
+        /**Взять банку по наклейке **/
+        try {
+            Vault.getJar(PicklesName.SALTBROCCOLI.toString());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        /**Положить банку в погреб **/
+        List<String> vegStr = new ArrayList<>();
+        vegStr.add(Vegetables.PEPPER.toString());
+        vegStr.add(Vegetables.POTATO.toString());
+        vegStr.add(Vegetables.TOMATO.toString());
+
+        Vault.setJar(PicklesName.ASSORTI.toString(), vegStr);
+
+        /** Проверяем наличие банок в погребе **/
+        Vault.showCountCellar();
+    }
+}
